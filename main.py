326467d20b2b4d2bfdcbdd4f9384dab34b2568f1
yehonatancohen <yoncohenyon@gmail.com>
2023-09-03 17:11:13 +0300

    showing the text on the image

--- main.py
+++ main.py
@@ -1,80 +1,83 @@
 import cv2
 import numpy as np
-from modules import morph, helper, otsu
+from modules import morph, helper
 import pytesseract
 from googletrans import Translator
+from bidi.algorithm import get_display
+
 
 translator = Translator()
  
-# Mention the installed location of Tesseract-OCR in your system
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-# Read image from which text needs to be extracted
-img = cv2.imread("image.png")
- 
-# Preprocessing the image starts
- 
-# Convert the image to gray scale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (7, 7), 0)
- 
-# Performing OTSU threshold
-ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
- 
-# Specify structure shape and kernel size.
-# Kernel size increases or decreases the area
-# of the rectangle to be detected.
-# A smaller value like (10, 10) will detect
-# each word instead of a sentence.
-rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
- 
-# Applying dilation on the threshold image
-dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
- 
-# Finding contours
-contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+def reverse_hebrew_rtl(text):
+    # Use the get_display function to convert the text to RTL format
+    rtl_text = get_display(text)
 
-# Create a copy of the original image to draw the contours on
-contour_img = np.copy(img)  # Replace 'original_image' with your input image variable
+    return rtl_text
 
-# Draw the contours on the contour_img
-cv2.drawContours(contour_img, contours, -1, (0, 255, 0), 2)  # Green color, thickness = 2
+def show_contours(img, contours):
+    # Draw the contours on the contour_img
+    contours_img = np.copy(img)
+    cv2.drawContours(contours_img, contours, -1, (0, 255, 0), 2)  # Green color, thickness = 2
 
-# Show the image with the contours
-#cv2.imshow('Contours', contour_img)
-#cv2.waitKey(0)
- 
-# Creating a copy of image
-im2 = img.copy()
- 
-# A text file is created and flushed
-file = open("recognized.txt", "w+")
-file.write("")
-file.close()
- 
-# Looping through the identified contours
-# Then rectangular part is cropped and passed on    
-# to pytesseract for extracting text from it
-# Extracted text is then written into the text file
-for cnt in contours:
-    x, y, w, h = cv2.boundingRect(cnt)
-     
-    # Drawing a rectangle on copied image
-    rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
-     
-    # Cropping the text block for giving input to OCR
-    cropped = im2[y:y + h, x:x + w]
-
-    # Open the file in append mode
-    file = open("recognized.txt", "a")
-    
-    # Apply OCR on the cropped image
-    text = pytesseract.image_to_string(cropped)
-    translated = translator.translate(text, src='en',dest='iw').text
-    print(text)
-    print(translated)
-    cv2.putText(img=img, text=translated, org=(x, y + int(h/2)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0, 0),thickness=2)
-    cv2.imshow("test",img)
+    # Show the image with the contours
+    cv2.imshow('Contours', contours_img)
     cv2.waitKey(0)
-     
-    file.close
+
+def translage_image(image_path, kernel_size):
+
+    img = cv2.imread("input/" + image_path)
+    # Convert the image to gray scalse
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+    # Blur the image for better result
+    blurred = cv2.GaussianBlur(gray, (7, 7), 0) 
+    
+    # Converting to black and white using OTSU algorithm
+    ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)    
+
+    # Create the kernel
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_size, kernel_size))
+    
+    # Applying dilation on the threshold image
+    dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
+    
+    # Finding contours
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    # Show the image with the contours
+    #show_contours(img, contours)
+    
+    # Creating a copy of image
+    im2 = img.copy()
+    
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+
+        # Drawing a rectangle on copied image
+        rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Cropping the text block for giving input to OCR
+        cropped = im2[y:y + h, x:x + w]
+
+        # Open the file in append mode
+        file = open("recognized.txt", "a")
+
+        # Apply OCR on the cropped image
+        text = pytesseract.image_to_string(cropped)
+        if text == "":
+            continue
+        translated = translator.translate(text, src='en',dest='iw').text
+        translated = reverse_hebrew_rtl(translated)
+        print(text)
+        print(translated)
+        img = helper.add_text_to_contour(img, translated, cnt)
+
+        file.close
+
+    cv2.imwrite("output/" + image_path, img)
+    cv2.imshow("Result",img)
+    cv2.waitKey(0)
+
+input_image = "example2.png"
+translage_image(input_image, 18)
--- modules/helper.py
+++ modules/helper.py
@@ -2,43 +2,57 @@ import cv2
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
-def find_best_matching_font_size(image_path, text, font_name="arial.ttf"):
-    image = Image.open(image_path)
-    draw = ImageDraw.Draw(image)
+def add_text_to_image_cv2(image, text, position, contour=None, font=cv2.FONT_HERSHEY_SIMPLEX,
+                          font_color=(255, 255, 255), font_thickness=1, background_color=(255, 255, 255)):
+    # Copy the input image
+    result = image.copy()
 
-    best_font_size = 1
-    best_score = float('inf')
+    # Convert the image to PIL format
+    pil_image = Image.fromarray(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(pil_image)
 
-    for font_size in range(1, 100):
-        font = ImageFont.truetype(font_name, size=font_size)
-        text_width, text_height = draw.textsize(text, font=font)
+    # Create a PIL font object (you can specify the font and size here)
+    font_size = 1
+    font = ImageFont.truetype("arial.ttf", font_size)
+    
+    # Calculate the size of the text with the given font
+    text_size = draw.textsize(text, font=font)
 
-        # Calculate the score based on the difference in text height between the image and the rendered text
-        score = abs(text_height - image.height)
+    # Extract width and height from the position tuple
+    x, y, width, height = position
 
-        if score < best_score:
-            best_score = score
-            best_font_size = font_size
+    # Determine the scaling factor to fit the text within the desired size
+    scaling_factor = min(width / text_size[0], height / text_size[1])
+    font_size = int(font_size * scaling_factor * 2)
 
-    return best_font_size, best_score
+    # Create the font with the adjusted size
+    font = ImageFont.truetype("arial.ttf", font_size)
 
-def add_text_with_best_matching_font(image_path, text, font_name="arial.ttf"):
-    best_font_size, _ = find_best_matching_font_size(image_path, text, font_name)
+    # Calculate the position to place the text
+    text_x, text_y = x, y
 
-    image = Image.open(image_path)
-    draw = ImageDraw.Draw(image)
+    # Add the text to the PIL image
+    
+    left, top, right, bottom = draw.textbbox((text_x, text_y), text, font=font)
+    draw.rectangle((left-5, top-5, right+5, bottom+5), fill="white")
+    draw.text((text_x, text_y), text, font=font, fill=font_color)
 
-    font = ImageFont.truetype(font_name, size=best_font_size)
+    # Convert the PIL image back to OpenCV format
+    result = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
 
-    # Calculate the position to center the text in the image
-    text_width, text_height = draw.textsize(text, font=font)
-    image_width, image_height = image.size
-    x = (image_width - text_width) // 2
-    y = (image_height - text_height) // 2
+    return result
 
-    draw.text((x, y), text, fill=(0, 0, 0), font=font)
+def add_text_to_contour(image, text, contour):
+    result = image.copy()
+    x, y, w, h = cv2.boundingRect(contour)
+    text_x = x 
+    text_y = y - h // 2
+    font_color = (0, 0, 0)
 
-    # Convert PIL image to OpenCV format
-    cv2_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 1
+    font_thickness = 4
 
-    return cv2_image
+    result = add_text_to_image_cv2(result, text, (text_x, text_y, w, h), contour, font, font_color, font_thickness)
+
+    return result
